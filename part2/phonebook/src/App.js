3e791e007a5a3cc90586af272d14c69cbe5becc7
Emily Peri <emily@emilyperi.com>
2022-07-18 12:56:12 -0700

    completed part 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,11 +1,18 @@
 import {useState} from 'react'
 
+const inputStyle = {
+  marginLeft: "5px",
+  marginBottom: "5px"
+}
+
 function App() {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: "800-123-4567" }, { name: 'Ada Lovelace', number: "666-666-6667"}
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchName, setSearchName] = useState('')
+  const [showAll, setShowAll] = useState(true)
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -32,14 +39,36 @@ function App() {
     setNewNumber(event.target.value)
   }
 
+  const handleSearchName = (event) => {
+    setSearchName(event.target.value)
+  }
+  const handleFilter = (event) => {
+    event.preventDefault()
+    setShowAll(!showAll)
+    if (!showAll) {
+      setSearchName('')
+    }
+  }
+
+  const personsToShow = showAll 
+    ? persons
+    : persons.filter(person => person.name.toLowerCase() === searchName.toLowerCase())
+
+
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <form onSubmit={handleFilter}>
+        <div>Search by name: <input style={inputStyle} value={searchName} onChange={handleSearchName}/></div>
+        <button type="submit">{showAll ? "search" : "clear"}</button>
+      </form>
+      <br/>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleNameChange}/>
+          name: <input style={inputStyle} value={newName} onChange={handleNameChange}/>
           <div>
-            number: <input value={newNumber} onChange={handleNumberChange}/></div>
+            number: <input style={inputStyle} value={newNumber} onChange={handleNumberChange}/></div>
         </div>
         <div>
           <button  type="submit">add</button>
@@ -48,7 +77,7 @@ function App() {
       <h2>Numbers</h2>
       <table>
         <tbody>
-      {persons.map(person => 
+      {personsToShow.map(person => 
         <tr key={person.name}>
           <td>{person.name}</td>
           <td>{person.number}</td>
